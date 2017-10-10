7d3c7faee0d410cdc90ac331e5323a0672f50d08
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2017-10-10 11:14:45 -0300

    [master] Aula 09 e Atividade 3

--- tarefas/atividade-3.py
+++ tarefas/atividade-3.py
@@ -1,13 +1,45 @@
 import cv2
 import numpy as np
 
+def mean_std(image, win):
+    height = image.shape[0]
+    width = image.shape[1]
+    
+    mean = np.ones(image.shape)
+    std = np.ones(image.shape)
+
+    for h in range(0, height, win):
+        for w in range(0, width, win):
+            mean[h:h+win, w:w+win]*=np.mean(image[h:h+win, w:w+win])
+            std[h:h+win, w:w+win]*=np.std(image[h:h+win, w:w+win])
+    return mean, std
+
+def threshold_sauvola(image, window_size, k, r=None):
+    if r is None: 
+        r = 0.5*(image.max()-image.min()) 
+    m, s = mean_std(image, window_size)
+    T = m * (1 + k * ((s / r) - 1))
+    return T
+
 img = cv2.imread("../db_images/png/captcha.png", 0)
 
-ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
-ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
-ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
-ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
+image = threshold_sauvola(img, window_size=3, k=0.999, r=128)
+cv2.imshow('image', image)
+cv2.waitKey(0)
+
+image = threshold_sauvola(img, window_size=7, k=0.999)
+cv2.imshow('image', image)
+cv2.waitKey(0)
+
+image = threshold_sauvola(img, window_size=5, k=0.05)
+cv2.imshow('image', image)
+cv2.waitKey(0)
+
+ret, thresh1 = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
+ret, thresh2 = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY_INV)
+ret, thresh3 = cv2.threshold(img, 50, 255, cv2.THRESH_TRUNC)
+ret, thresh4 = cv2.threshold(img, 50, 255, cv2.THRESH_TOZERO)
+ret, thresh5 = cv2.threshold(img, 50, 255, cv2.THRESH_TOZERO_INV)
 
 threshN = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
 
