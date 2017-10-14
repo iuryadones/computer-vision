21a7460f2776d70db455e51ed1b53ca395fa17eb
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2017-10-13 21:06:53 -0300

    [master] aula 10 - classify_digits and mnist

---
+++ aula_10/classify_digits.py
@@ -0,0 +1,44 @@
+import os
+from collections import Counter
+
+import cv2
+import numpy as np
+from skimage.feature import hog
+from sklearn.externals import joblib
+
+path = os.getcwd() + os.sep
+path += '../db_aulas/Imagens/numbers.jpg'
+
+clf = joblib.load('digits_cls.pkl')
+
+im = cv2.imread(path)
+cv2.imshow('Digits Original', im)
+cv2.waitKey(0)
+
+gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5,5), 0.)
+
+ret, im_th = cv2.threshold(gray,127,255,0)
+im2,cnts,hierarchy = cv2.findContours(im_th, 1, 2)
+
+rects = [cv2.boundingRect(cnt) for cnt in cnts]
+for rect in rects:
+    leng = int(rect[3] * 1.6)
+    pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+    pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+
+    roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
+    roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+    roi = cv2.dilate(roi, (3, 3))
+
+    cv2.imshow("Roi", roi)
+    cv2.waitKey(500)
+
+    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1))
+    nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
+
+    cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
+    cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+
+cv2.imshow("Resulting Image with Rectangular ROIs", im)
+cv2.waitKey(0)
--- aula_10/mnist.py
+++ aula_10/mnist.py
@@ -1,10 +1,10 @@
+from collections import Counter
+
+import numpy as np
+from skimage.feature import hog
 from sklearn.externals import joblib
 from sklearn.datasets.mldata import fetch_mldata
-from skimage.feature import hog
 from sklearn.svm import LinearSVC
-import numpy as np
-from collections import Counter
-import cv2
 
 dataset = fetch_mldata('mnist-original', data_home='./MNIST')
 
@@ -23,8 +23,6 @@ print("Count of digits in dataset", Counter(labels))
 clf = LinearSVC()
 clf.fit(hog_features, labels)
 
-# joblib.dump(clf, 'digits_cls.pkl', compress=3)
+joblib.dump(clf, 'digits_cls.pkl', compress=3)
 
-img = cv2.imread('/home/iury/Personal/UFRPE/Mestrado_IA/VISAO/db_aulas/Imagens/numbers.jpg')
-cv2.imshow('Digits Original', img)
-cv2.waitKey(0)
+clf = joblib.load('digits_cls.pkl')
