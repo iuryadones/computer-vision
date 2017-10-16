295834a69f62a9b774fec50d828602d019f8fcb3
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2017-10-16 08:15:32 -0300

    [master] Add imshow processing images

--- aula_10/classify_digits.py
+++ aula_10/classify_digits.py
@@ -16,14 +16,19 @@ cv2.imshow('Digits Original', im)
 cv2.waitKey(0)
 
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5,5), 0.)
+cv2.imshow('Original Gray', gray)
+cv2.waitKey(0)
 
-ret, im_th = cv2.threshold(gray,127,255,0)
+gray = cv2.GaussianBlur(gray, (21, 21), 0.5)
+cv2.imshow('GaussianBlur Gray', gray)
+cv2.waitKey(0)
+
+ret, im_th = cv2.threshold(gray, 127, 255, 0)
 im2,cnts,hierarchy = cv2.findContours(im_th, 1, 2)
 
 rects = [cv2.boundingRect(cnt) for cnt in cnts]
 for rect in rects:
-    leng = int(rect[3] * 1.6)
+    leng = int(rect[3] * 1.)
     pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
     pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
 
@@ -32,7 +37,7 @@ for rect in rects:
     roi = cv2.dilate(roi, (3, 3))
 
     cv2.imshow("Roi", roi)
-    cv2.waitKey(500)
+    cv2.waitKey(200)
 
     roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1))
     nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
