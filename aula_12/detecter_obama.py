7107dc913a7012cc854d9b3839c63553a7250391
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2017-10-20 15:40:10 -0300

    [master] aula 12 Distance zero de comparação para obama

--- aula_12/detecter_obama.py
+++ aula_12/detecter_obama.py
@@ -16,31 +16,31 @@ kp, des = sift.detectAndCompute(img, None)
 path2 = os.getcwd() + os.sep
 path2 += '../db_aulas/Imagens/obama_knn.jpg'
 
-img2 = cv2.imread(path2)
-img2 = imutils.resize(img2, width=500)
+img2 = cv2.imread(path)
+# img2 = imutils.resize(img2, width=500)
 
 sift2 = cv2.xfeatures2d.SIFT_create()
 kp2, des2 = sift2.detectAndCompute(img2, None)
 
 im_with_keypoists = cv2.drawKeypoints(img, kp, np.array([]), (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 cv2.imshow("Keypoints", im_with_keypoists)
-cv2.waitKey()
+cv2.waitKey(0)
 
-kpts = sorted(kp, key=lambda x: x.response)[-5::]
-print(kpts)
+#kp = sorted(kp, key=lambda x: x.response)[-5::]
+#print(kp)
 
-im_with_keypoists = cv2.drawKeypoints(img, kpts, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+im_with_keypoists = cv2.drawKeypoints(img, kp, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 cv2.imshow("Keypoints", im_with_keypoists)
-cv2.waitKey()
-
+cv2.waitKey(0)
 
 bf = cv2.BFMatcher()
 matches = bf.knnMatch(des, des2, k=2)
 
 good = []
 for m,n in matches:
-    if m.distance < 0.90*n.distance:
+    if m.distance < 0.75*n.distance:
         good.append([m])
+        print(m.distance)
 
 img3 = cv2.drawMatchesKnn(img, kp, img2, kp2, good, None, flags=2)
 cv2.imshow("Matches Knn", img3)
