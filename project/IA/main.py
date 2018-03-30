74a43df8bb4bc0076ef08557200aa952432125d1
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2018-03-30 12:10:57 -0300

    [master] Modific IA

--- project/IA/main.py
+++ project/IA/main.py
@@ -5,37 +5,45 @@ from skimage.feature import hog
 from sklearn.datasets.mldata import fetch_mldata
 from sklearn.externals import joblib
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 import cv2
 import numpy as np
 
 
 def loader_data(origin):
     dataset = fetch_mldata(origin, data_home='./train')
-    features = np.array(dataset.data, 'int16')
-    labels = np.array(dataset.target, 'int')
+    features = np.array(dataset.data, 'int32')
+    labels = np.array(dataset.target, 'int8')
     print("Count of digits in dataset", Counter(labels))
 
     return features, labels
 
 
 def feature_hog(features, labels):
-    list_hog_fd = []
-    for feature in features:
-        fd = hog(feature.reshape((28, 28)),
-                 orientations=9,
-                 pixels_per_cell=(14, 14),
-                 cells_per_block=(1, 1),
-                 block_norm='L2-Hys')
-        list_hog_fd.append(fd)
-    hog_features = np.array(list_hog_fd, 'float64')
+    kwargs = {
+        'orientations': 9,
+        'pixels_per_cell': (8, 8),
+        'cells_per_block': (3, 3),
+        'block_norm': 'L2-Hys',
+        'transform_sqrt': True
+    }
+
+    list_hog_fd = map(
+        lambda feature: hog(feature.reshape((28, 28)), **kwargs), features
+    )
+
+    hog_features = np.array(list(list_hog_fd), 'float64')
     return hog_features
 
 
 def dump_clf(name_pkl, hog_features, labels):
     clf = LinearSVC()
+    print('Start trainng')
     clf.fit(hog_features, labels)
+    print('End trainng')
+    print('Start Dump')
     joblib.dump(clf, name_pkl, compress=3)
-
+    print('End Dump')
 
 def classify(name_pkl):
     way = os.getcwd() + os.sep
@@ -46,28 +54,32 @@ def classify(name_pkl):
     for test in testing:
         path = way + test
         im = cv2.imread(path)
-        cv2.imshow('Digits Original', im)
-        cv2.waitKey(200)
-        cv2.destroyAllWindows()
+
+        # cv2.imshow('Digits Original', im)
+        # cv2.waitKey(200)
+        # cv2.destroyAllWindows()
 
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Original Gray', gray)
-        cv2.waitKey(200)
-        cv2.destroyAllWindows()
 
-        for _ in range(5):
-            gray = cv2.GaussianBlur(gray, (21, 21), 0.5)
-            cv2.imshow('GaussianBlur Gray', gray)
-        cv2.waitKey(200)
-        cv2.destroyAllWindows()
+        # cv2.imshow('Original Gray', gray)
+        # cv2.waitKey(200)
+        # cv2.destroyAllWindows()
+
+        for _ in range(1):
+            gray = cv2.GaussianBlur(gray, (5, 5), 0.0)
+            # gray = cv2.medianBlur(gray, 3)
+            # cv2.imshow('GaussianBlur Gray', gray)
+            # cv2.waitKey(200)
+            # cv2.destroyAllWindows()
 
         ret, im_th = cv2.threshold(gray,
                                    0,
                                    255,
                                    cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        cv2.imshow('THRESH_BINARY_INV and THRESH_OTSU', im_th)
-        cv2.waitKey(200)
-        cv2.destroyAllWindows()
+
+        # cv2.imshow('THRESH_BINARY_INV and THRESH_OTSU', im_th)
+        # cv2.waitKey(200)
+        # cv2.destroyAllWindows()
 
         im2, cnts, hierarchy = cv2.findContours(im_th,
                                                 cv2.RETR_EXTERNAL,
@@ -90,19 +102,23 @@ def classify(name_pkl):
 
                 roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
                 roi = cv2.resize(roi, (28, 28), interpolation=ntr)
+                # roi = cv2.erode(roi, (3, 3))
+                # roi = cv2.dilate(roi, (3, 3))
 
                 cv2.imshow("Roi", roi)
-                cv2.waitKey(100)
+                cv2.waitKey(200)
                 cv2.destroyAllWindows()
 
                 roi_hog_fd = hog(roi,
                                  orientations=9,
-                                 pixels_per_cell=(14, 14),
-                                 cells_per_block=(1, 1),
-                                 block_norm='L2-Hys')
+                                 pixels_per_cell=(8, 8),
+                                 cells_per_block=(3, 3),
+                                 block_norm='L2-Hys',
+                                 transform_sqrt=True)
 
-                vector_predict.append(
-                    clf.predict(np.array([roi_hog_fd], 'float64')))
+                pred = clf.predict(np.array([roi_hog_fd], 'float64'))
+                print(pred)
+                vector_predict.append(pred)
 
             nbr = Counter([v[0] for v in vector_predict]).most_common()
             print(nbr)
@@ -127,16 +143,17 @@ def classify(name_pkl):
         cv2.destroyAllWindows()
 
 
-def main():
+def main(trainnig=False):
     name_pkl = 'digits_clf.pkl'
     dataset = 'mnist-original'
 
-    features, labels = loader_data(dataset)
-    hog_features = feature_hog(features, labels)
-    if not os.path.exists(name_pkl):
+    if trainnig:
+        features, labels = loader_data(dataset)
+        hog_features = feature_hog(features, labels)
         dump_clf(name_pkl, hog_features, labels)
+
     classify(name_pkl)
 
 
 if __name__ == "__main__":
-    main()
+    main(True)
