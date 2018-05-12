477dde4e1aab85f0f864db80e1e38335c9565adb
Iury Adones Xavier dos Santos <iuryadones@gmail.com>
2018-05-12 13:29:52 -0300

    [master] base digits

--- project/vision_ia.py
+++ project/vision_ia.py
@@ -1,3 +1,9 @@
+import io
+from io import StringIO
+from io import BytesIO
+
+import os
+
 import cv2
 import numpy as np
 import pylab as plt
@@ -5,10 +11,6 @@ import pandas as pd
 
 import PIL
 from PIL import Image
-
-import io
-from io import StringIO
-from io import BytesIO
 
 import matplotlib.pyplot as plt
 
@@ -27,30 +29,49 @@ settings = {
 
 connect = MongoClient(**settings['client'])
 db = connect.get_database(**settings['db'])
-xcoll = 'fs.chunks'
+xcoll_1 = 'fs.chunks'
+xcoll_2 = 'captchas'
 
-print(f'Captchas\nTotal: {db[xcoll].count()}')
-data = list(db[xcoll].find({}).skip(547517).limit(1))[0]['data']
+print(f'Captchas\nTotal: {db[xcoll_1].count()}')
 
-pp = PIL.Image.open(BytesIO(data))
 
-cv2.imshow('image', np.array(pp))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+for info in db[xcoll_2].find({}):
+    if info['correct'] and info['answer'].isdigit():
+        print(info['answer'])
+        print(info['correct'])
+        print(info['_id'])
+
+        data = db[xcoll_1].find({'files_id':info['_id']})[0]
+
+        d = data['data']
+
+        pp = PIL.Image.open(BytesIO(d))
+
+        path = f"../db_images/captcha/{info['answer']}"
+
+        if not os.path.exists(path):
+            os.mkdir(path)
+        else:
+            pp.save(f'{path}/sample-{len(os.listdir(path)) + 1}.png',
+                    format='png')
+
+        cv2.imshow('image', np.array(pp))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 # pp.save('./captcha.png',format='png')
-plt.figure()
-plt.xticks([])
-plt.yticks([])
-plt.imshow(pp)
-plt.tight_layout()
-plt.show()
+# plt.figure()
+# plt.xticks([])
+# plt.yticks([])
+# plt.imshow(pp)
+# plt.tight_layout()
+# plt.show()
 
-image = cv2.imread('../db_images/jpeg/captcha.jpeg')
-plt.figure()
-plt.title("BGR")
-plt.xticks([])
-plt.yticks([])
-plt.imshow(image)
-plt.tight_layout()
-plt.show()
+# image = cv2.imread('../db_images/jpeg/captcha.jpeg')
+# plt.figure()
+# plt.title("BGR")
+# plt.xticks([])
+# plt.yticks([])
+# plt.imshow(image)
+# plt.tight_layout()
+# plt.show()
